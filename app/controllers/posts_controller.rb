ddf42d0df47f906d66120885680bf85580f12553
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-06-29 20:47:50 -0400

    Create post and user model using devise

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+end
